50492b81a1cd259427b9e524ec026e095d30e5eb
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-20 23:49:13 +0200

    Component using props

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+const data = [
+  {id: 1, name: "Virgi", email: "virgi@gmail.com"},
+  {id: 2, name: "Pepe", email: "pepe@gmail.com"},
+  {id: 3, name: "Toni", email: "toni@gmail.com"},
+];
+
+// Component gets props as parameter
+const Contact = props =>
+  <div className='pure-u-1-3'>
+    <h2>{props.name}</h2>
+    <p>{props.email}</p>
+  </div>
+
 class Layout extends React.Component {
   render() {
     return (
       <div id='Layout' className='pure-g'>
-        <div className='pure-u-1-3'>Pepe</div>
-        <div className='pure-u-1-3'>Toni</div>
-        <div className='pure-u-1-3'>Jose</div>
+      {data.map(info =>
+        // Mapping each property in props 
+        // <Contact id={info.id} name={info.name} email={info.email} />
+
+        // splat operator to pass the props
+        <Contact {...info}/>
+      )}
       </div>
     );
   }
