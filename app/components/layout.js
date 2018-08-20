a6a356f099fba8bd3c305873fb9fd5245e0a98b1
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-20 23:29:51 +0200

    Pure css library

--- app/components/layout.js
+++ app/components/layout.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Layout extends React.Component {
   render() {
     return (
-      <div id='Layout'>
-        Hello from Layout!
+      <div id='Layout' className='pure-g'>
+        <div className='pure-u-1-3'>Pepe</div>
+        <div className='pure-u-1-3'>Toni</div>
+        <div className='pure-u-1-3'>Jose</div>
       </div>
     );
   }
