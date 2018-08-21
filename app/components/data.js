a4a416323fd84c457758cf3f628b2d32d6c8cfe1
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-21 22:21:30 +0200

    Data extracted to a file

---
+++ app/components/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, name: "Virgi", email: "virgi@gmail.com" },
+  { id: 2, name: "Pepe", email: "pepe@gmail.com" },
+  { id: 3, name: "Toni", email: "toni@gmail.com" },
+];
+
+export default data;
