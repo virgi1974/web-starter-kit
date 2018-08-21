015707688c7d1f2f4e79a1bb8519641b823f91cb
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-21 23:33:20 +0200

    Adds html form using a function

--- app/components/layout.js
+++ app/components/layout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Contact from './contact';
 import data from './data';
 
+import './layout.css'
+
 class Layout extends React.Component {
 
   componentWillMount() {
@@ -24,9 +26,26 @@ class Layout extends React.Component {
     })
   }
 
+  newContact = () =>
+    <div className='pure-g'>
+      <div className='pure-u-12-24'>
+        <form className="pure-form">
+          <fieldset>
+            <legend>New Contact</legend>
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Name" />
+
+            <button type="submit" className="pure-button pure-button-primary">Add</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>; 
+// must end using ; because is a function (like jsx)
+
   render() {
     return (
       <div id='Layout'>
+        {this.newContact()}
         <a href='#' className='pure-button' onClick={this.addContact}>Add Contact</a>
         <div id='Layout' className='pure-g'>
           {this.state.contacts.map(info =>
