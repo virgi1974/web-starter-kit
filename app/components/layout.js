83f84405509824abdb6b4b5f7b2cf8199c67119e
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-21 22:48:31 +0200

    Adds button and widens the main container div

--- app/components/layout.js
+++ app/components/layout.js
@@ -3,16 +3,21 @@ import Contact from './contact';
 import data from './data';
 
 class Layout extends React.Component {
+
+  addContact = (e) => {
+    e.preventDefault();
+    console.log('clicked');
+  }
+
   render() {
     return (
-      <div id='Layout' className='pure-g'>
-      {data.map(info =>
-        // Mapping each property in props 
-        // <Contact id={info.id} name={info.name} email={info.email} />
-
-        // splat operator to pass the props
-        <Contact key={info.id} {...info}/>
-      )}
+      <div id='Layout'>
+        <a href='#' className='pure-button' onClick={this.addContact}>Add Contact</a>
+        <div id='Layout' className='pure-g'>
+          {data.map(info =>
+            <Contact key={info.id} {...info}/>
+          )}
+        </div>
       </div>
     );
   }
