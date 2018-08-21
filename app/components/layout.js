5f02efb54f70f24dea110cf926eb73d1faf2065f
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-21 22:22:17 +0200

    Imports data and contact component

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,17 +1,6 @@
 import React from 'react';
-
-const data = [
-  {id: 1, name: "Virgi", email: "virgi@gmail.com"},
-  {id: 2, name: "Pepe", email: "pepe@gmail.com"},
-  {id: 3, name: "Toni", email: "toni@gmail.com"},
-];
-
-// Component gets props as parameter
-const Contact = props =>
-  <div className='pure-u-1-3'>
-    <h2>{props.name}</h2>
-    <p>{props.email}</p>
-  </div>
+import Contact from './contact';
+import data from './data';
 
 class Layout extends React.Component {
   render() {
